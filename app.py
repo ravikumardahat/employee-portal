d8c531f0f3eceb66fa57f1f90ba13d03fc81da9a
Ravikumar Dahat <ravikumardahat@gmail.com>
2026-06-08 17:35:43 +0000

    Updated flask with mysql

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from flask import Flask
+import mysql.connector
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Employee Portal"
+
+    conn = mysql.connector.connect(
+        host="mysql",
+        user="root",
+        password="root123",
+        database="employee_db"
+    )
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT NOW()")
+
+    result = cursor.fetchone()
+
+    conn.close()
+
+    return f"MySQL Connected: {result}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
